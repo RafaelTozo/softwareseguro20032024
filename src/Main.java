78ce8eb527f66226d2b971a7aae85707d7e3f4d2
Rafael de Lima Tozo <rafaeldelimatozo@gmail.com>
2024-03-20 21:00:23 -0300

    sh: line 1: qq: command not found

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI");
+        System.out.println("Página de login");
+        System.out.println("Usuário: admin");
+        System.out.println("Senha: **********");
     }
 }
