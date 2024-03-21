9395e2f539acc13dfacedd274fd86fc36d766f17
Rafael de Lima Tozo <rafaeldelimatozo@gmail.com>
2024-03-20 21:15:29 -0300

    pagina de cadastro

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI");
+        System.out.println("Página de cadastro");
+        System.out.println("Usuário: admin");
+        System.out.println("Sennha: *****");
+        System.out.println("CPF: 123123123");
+        System.out.println("Email: sugma@balls");
+        System.out.println("->Cadastrar<-");
     }
 }
